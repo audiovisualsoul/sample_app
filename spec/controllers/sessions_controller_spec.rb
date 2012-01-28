c4ef5d33a46e406be3baf5171f4aa076035ac969
Sean Flynn <seanflynn@comcast.net>
2012-01-28 04:27:56 -0500

    Created sign in cookie

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,7 @@ describe SessionsController do
 
 render_views
 
-  describe "GET 'new'" do
+	describe "GET 'new'" do
   
     it "returns http success" do
       get :new
@@ -38,6 +38,25 @@ render_views
   			it "should have an error message" do
   				post :create, :session => @attr
   				flash.now[:error].should =~ /invalid/i
+  			end
+  		end
+  		
+  		describe "success" do
+  			
+  			before(:each) do
+  				@user = Factory(:user)
+  				@attr = { :email => @user.email, :password => @user.password }
+  			end
+  			
+  			it "should sign the user in" do
+  				post :create, :session => @attr
+  				controller.current_user.should == @user
+  				controller.should be_signed_in
+  			end
+  			
+  			it "should redirect to the user show page" do
+  				post :create, :session => @attr
+  				response.should redirect_to(user_path(@user))
   			end
   		end
 	end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 Factory.define :user do |user|
-	user.name					"Sean Flynn"
-	user.email					"sean@example.com"
+	user.name					"Harry Henderson"
+	user.email					"harry@harry.com"
 	user.password						"foobar"
 	user.password_confirmation "foobar"
 	
