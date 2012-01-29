bd60fbc3e8a2aa2f1eb290216213ea266dce703c
Sean Flynn <seanflynn@comcast.net>
2012-01-28 22:49:26 -0500

    lilengine could

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,22 +9,26 @@ module SessionsHelper
 		@current_user = user
 	end
 	
-	def current_user
-		@current_user || user_from_remember_token
-	end
+	def signed_in?
+    	!current_user.nil?
+  	end
 	
-	  def signed_in?
-    !current_user.nil?
+  def current_user
+    @current_user ||= user_from_remember_token
   end
-	
-private
-	
-	def user_from_remember_token
-		User.authenticate_with_salt( remember_token )
-	end
-	
-	def remember_token
-		cookies.signed[:remember_token] || [nil, nil]
-	end
+  
+	def sign_out
+   		cookies.delete(:remember_token)
+    	self.current_user = nil
+  	end
+  
+  private
+  
+    def user_from_remember_token
+      User.authenticate_with_salt(*remember_token)
+    end
+    
+    def remember_token
+      cookies.signed[:remember_token] || [nil, nil]
+    end
 end
-
